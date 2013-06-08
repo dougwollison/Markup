8443085c4f1823dc5a3ed5bfeb6d838a26b9e7ba
Doug Wollison <doug@wollison.net>
2013-06-08 16:58:30 -0400

    Added overloading method and primary atts list

--- HTML.php
+++ HTML.php
@@ -2,10 +2,14 @@
 namespace Markup;
 
 class HTML{
-	//Tags that are self closing
+	/**
+	 * All tags that should contain no content; they close themselves
+	 */
 	protected static $selfclosing_tags = array('area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr');
 
-	//All allowed attributes per tag
+	/**
+	 * All allowed attributes per tag
+	 */
 	protected static $allowed_attributes = array(
 		//Global attributes
 		0 => array('accesskey', 'class', 'contenteditableNew', 'contextmenuNew', 'dir', 'draggableNew', 'dropzoneNew', 'hiddenNew', 'id', 'lang', 'spellcheckNew', 'style', 'tabindex', 'title', 'translate', 'onafterprint', 'onbeforeprint', 'onbeforeunload', 'onerror', 'onhaschange', 'onload', 'onmessage', 'onoffline', 'ononline', 'onpagehide', 'onpageshow', 'onpopstate', 'onredo', 'onresize', 'onstorage', 'onundo', 'onunload'),
@@ -59,10 +63,41 @@ class HTML{
 		'video' => array('autoplay', 'controls', 'height', 'loop', 'muted', 'poster', 'preload', 'src', 'width'),
 		'object' => array('data', 'form', 'height', 'name', 'type', 'usemap', 'width'),
 	);
-	
-	//Attributes that serve as boolean values
+
+	/**
+	 * Attributes that serve as boolean values
+	 */
 	protected static $boolean_atts = array('allowfullscreen', 'autofocus', 'autoplay', 'checked', 'controls', 'muted', 'disabled', 'formnovalidate', 'loop', 'multiple', 'novalidate', 'readonly', 'required', 'selected');
-	
+
+	/**
+	 * For overloading methods: the directly settable attributes and their defaults
+	 */
+	protected static $primary_atts = array(
+		'a' => array('href', 'target'),
+		'button' => array('type' => 'button', 'name', 'value'),
+		'iframe' => array('src', 'width', 'height'),
+		'img' => array('src', 'width', 'height', 'alt'),
+		'input' => array('name', 'value', 'type' => 'text'),
+		'label' => array('for'),
+		'link' => array('href', 'rel' => 'stylesheet'),
+		'meta' => array('name', 'content', 'scheme'),
+		'optgroup' => array('label'),
+		'option' => array('value'),
+		'script' => array('src'),
+		'select' => array('name', 'value'),
+		'style' => array('type' => 'text/css'),
+		'div' => array('class')
+	);
+
+	/**
+	 * Build an HTML element and return the string of it
+	 *
+	 * @param string $tag The name of the tag to build
+	 * @param string|array $content The content of the tag ($atts can be passed here instead)
+	 * @param array $atts The list of attributes for the tag
+	 *
+	 * @return string $html The HTML of the tag
+	 */
 	public static function build($tag, $content = null, $atts = null){
 		//Check if an array was passed as $content, if so, swap with $atts
 		if(is_array($content)){
@@ -116,8 +151,89 @@ class HTML{
 		return $html;
 	}
 
-	//Echo the result of the HTML::build()
+	/**
+	 * Echo the result of the HTML::build()
+	 *
+	 * @param string $tag The name of the tag to build
+	 * @param string|array $content The content of the tag ($atts can be passed here instead)
+	 * @param array $atts The list of attributes for the tag
+	 */
 	public static function render($tag, $content = null, $atts = null){
 		echo self::build($tag, $content, $atts);
+	}
+	
+	/**
+	 * Overloading; dynamically build tag processing arguments based on method name
+	 *
+	 * Uses HTML::$primary_atts as basis for what arguments go to what attributes
+	 * 
+	 * Tags that can hold content must have their content passed first,
+	 * then the primary attributes in appropriate order, followed at last
+	 * by an array of additional attributes if needed
+	 *
+	 * Examples:
+	 *		HTML::a('my link', 'http://mysite.com', '_blank')
+	 *			<a href="http://mysite.com" target="_blank">my link</a>
+	 *		HTML::input('myfield', 'myvalue', 'checkbox')
+	 *			<input name="myfield" value="myvalue" type="checkbox">
+	 *		HTML::input('myfield', 'myvalue')
+	 *			<input name="myfield" value="myvalue" type="text">
+	 *		HTML::button('Submit', 'submit')
+	 *			<button type="checkbox">Submit</button>
+	 *		HTML::input('Click Me')
+	 *			<button type="button">Click Me</button>
+	 *		HTML::label('My Field', 'myfield')
+	 *			<label for="myfield">My Field</label>
+	 *		HTML::style('*{margin:0}');
+	 *			<style type="text/css">*{margin:0}</style>
+	 *		HTML::div('My content', 'myclass', array('id' => 'myid'))
+	 *			<div class="myclass" id="myid">My content</div>
+	 *
+	 * @param string $name The name of the method called
+	 * @param array $arguments The list of arguments passed
+	 */
+	public static function __callStatic($name, $arguments){
+		//Check if it's a tag with primary attributes
+		if(in_array($name, self::$primary_atts)){
+			$primaries = self::$primary_atts[$name];
+		
+			//If not a self closing tag, first argument must be content
+			if(!in_array($tag, self::$self_closing)){
+				$content = array_shift($arguments);
+			}else{
+				$content = null;
+			}
+			
+			$atts = array();
+			//Run through the $defaults
+			foreach($primaries as $primary => $default){
+				if(is_numeric($primary)){
+					$primary = $default;
+					$default = null;
+				}
+				
+				//Set the default attribute
+				$atts[$primary] = $default;
+				
+				//See if there is anything left in $arguments
+				if($arguments){
+					$arg = array_shift($arguments);
+					
+					//Check if it's an array, make that the rest of $atts if so
+					if(is_array($arg)){
+						$atts = array_merge($atts, $arg);
+						break;
+					}else{
+						//Otherwise, assign the value to the current primary attribute
+						$atts[$primary] = array_shift($arguments);
+					}
+				}
+			}
+		}else{
+			if(isset($arguments[0])) $content = $arguments[0];
+			if(isset($arguments[1])) $atts = $arguments[1];
+		}
+		
+		return self::build($name, $content, $atts);
 	}
 }
